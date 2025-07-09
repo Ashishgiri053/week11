81a2798d51a91c04766b860b6593b840c1aa3329
Ashishgiri053 <88329458+Ashishgiri053@users.noreply.github.com>
2025-07-09 17:33:48 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -66,14 +66,14 @@ resource "aws_security_group" "web-sg" {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-cidr_blocks = ["99.227.4.111/32"]
+    cidr_blocks = ["99.227.4.111/32"]
   }
   // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-cidr_blocks = ["99.227.4.111/32"]
+    cidr_blocks = ["99.227.4.111/32"]
   }
 }
 
